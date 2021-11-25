c1a67c7132edc7a7a65acfdc90d40d42ba8f1a90
Terrac Skiens <terracnosaur@gmail.com>
2021-11-25 06:24:42 -0800

    ceph works, prometheus doesnt

--- create_namespaces.sh
+++ create_namespaces.sh
@@ -1,5 +1,5 @@
 for NS in \
-    flannel httpbin istio-system k8sdash kube-dash lidarr loki metallb \
+    flannel httpbin istio-system k8sdash kube-dash lidarr loki metallb rook-ceph \
     monitoring nfs nginx nzbget plex radarr sonarr tautulli tdarr ubuntu ;
     do
         kubectl create ns ${NS}
