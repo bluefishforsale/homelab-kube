d7c99ac15d0f122c9ef0af2207b4901cf02f6a37
Terrac Skiens <terracnosaur@gmail.com>
2021-11-25 11:11:42 -0800

    plex, sonarr, radarr all on CEPH

--- create_namespaces.sh
+++ create_namespaces.sh
@@ -1,6 +1,6 @@
 for NS in \
     flannel httpbin istio-system k8sdash kube-dash lidarr loki metallb rook-ceph \
-    monitoring nfs nginx nzbget plex radarr sonarr tautulli tdarr ubuntu ;
+    monitoring nfs nginx nzbget plex radarr sonarr tautulli tdarr ubuntu media ;
     do
         kubectl create ns ${NS}
     done
