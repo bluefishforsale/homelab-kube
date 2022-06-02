810d49a4b1dfda65a1159e803fca5b137ae822ef
Terrac Skiens <3721396+bluefishforsale@users.noreply.github.com>
2022-06-02 10:18:39 -0700

    add .helmignore to all

--- charts/plex/plex-logs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kubectl exec -n media $(kubectl get pods -n media -l app.kubernetes.io/instance=plex  --output=jsonpath={.items..metadata.name}) -c plex -- ls -l "/config/Library/Application Support/Plex Media Server/Logs"
--- charts/plex/plex-transcode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl exec -n media $(kubectl get pods -n media -l app.kubernetes.io/instance=plex  --output=jsonpath={.items..metadata.name}) -c plex --  \
- du -hs /transcode
-kubectl exec -n media $(kubectl get pods -n media -l app.kubernetes.io/instance=plex  --output=jsonpath={.items..metadata.name}) -c plex --  \
- find "/transcode" -type f | head -n20
